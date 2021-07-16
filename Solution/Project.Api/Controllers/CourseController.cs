6e4761f8e32597cb4ac7f93c834f02acb5d9e478
Eduardo Ñustez <eduardonustez@gmail.com>
2021-07-16 18:17:56 -0500

    Agregar Api Project

---
+++ Solution/Project.Api/Controllers/CourseController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Project.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController : ControllerBase
+    {
+       
+    }
+}
