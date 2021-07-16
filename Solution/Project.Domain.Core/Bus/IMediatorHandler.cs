ee3b3b7bec63199a6ee726444938c02f6f54e916
Eduardo Ñustez <eduardonustez@gmail.com>
2021-07-16 17:23:10 -0500

    Agregar Domain.Core e implementar InMemoryBus

---
+++ Solution/Project.Domain.Core/Bus/IMediatorHandler.cs
@@ -0,0 +1,13 @@
+﻿using Project.Domain.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Domain.Core.Bus
+{
+    public interface IMediatorHandler
+    {
+        Task SendCommand<T>(T command) where T : Command;
+    }
+}
--- Solution/Prooject.Infra.Bus/InMemoryBus.cs
+++ Solution/Prooject.Infra.Bus/InMemoryBus.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using MediatR;
+using Project.Domain.Core.Bus;
+using Project.Domain.Core.Commands;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Project.Infra.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus : IMediatorHandler
     {
+        private readonly IMediator _mediator;
 
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public Task SendCommand<T>(T command) where T: Command
+        {
+            return _mediator.Send(command);
+        }
     }
 }
