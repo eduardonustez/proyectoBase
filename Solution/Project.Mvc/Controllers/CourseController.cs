3027ec34619caa227c84d2a50abfef5388149b11
Eduardo Ñustez <eduardonustez@gmail.com>
2021-07-16 16:44:32 -0500

    Agregar vista y controlador para Course

---
+++ Solution/Project.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Project.Application.Interfaces;
+using Project.Application.ViewModels;
+
+namespace Project.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model= _courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
