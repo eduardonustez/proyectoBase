1ae472daf9cd964519f6097a1cc8d27596fc8e86
Eduardo Ñustez <eduardonustez@gmail.com>
2021-07-16 18:13:09 -0500

    Agregar funcionalidad Crear Curso

--- Solution/Project.Application/Interfaces/ICourseService.cs
+++ Solution/Project.Application/Interfaces/ICourseService.cs
@@ -8,5 +8,6 @@ namespace Project.Application.Interfaces
     public interface ICourseService
     {
         CourseViewModel GetCourses();
+        void Create(CourseViewModel courseViewModel);
     }
 }
--- Solution/Project.Application/Services/CourseService.cs
+++ Solution/Project.Application/Services/CourseService.cs
@@ -1,5 +1,7 @@
 ﻿using Project.Application.Interfaces;
 using Project.Application.ViewModels;
+using Project.Domain.Commands;
+using Project.Domain.Core.Bus;
 using Project.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -9,11 +11,24 @@ namespace Project.Application.Services
 {
     public class CourseService : ICourseService
     {
-        private ICourseRepository _courseRepository;
-        public CourseService(ICourseRepository courseRepository)
+        private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _bus;
+        public CourseService(ICourseRepository courseRepository,IMediatorHandler bus)
         {
             _courseRepository = courseRepository;
+            _bus = bus;
         }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            var createCourseCommand = new CreateCourseCommand(
+                courseViewModel.Name,
+                courseViewModel.Description,
+                courseViewModel.ImageUrl
+                );
+            _bus.SendCommand(createCourseCommand);
+        }
+
         public CourseViewModel GetCourses()
         {
             return new CourseViewModel()
